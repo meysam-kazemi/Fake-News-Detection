a0c665333e3a01c4fa0fd8f72f0b224d961f5a9c
meysam-kazemi <meisam.daysu@gmail.com>
2024-03-11 00:23:50 +0330

    change color

--- detection.py
+++ detection.py
@@ -1,17 +1,18 @@
 import pickle
 from utils import text_with_title, load_vectorizer
 
+
 class detection:
     def __init__(self):
-        with open('model.pkl', 'rb') as f:
+        with open("model.pkl", "rb") as f:
             self.model = pickle.load(f)
-        print('\033[33mModel Loaded!\033[00m')
+        print("\033[33mModel Loaded!\033[00m")
 
         self.vect = load_vectorizer()
 
-        self.classes = {1.:"Fake", 0.:"Real"}
+        self.classes = {1.0: "Fake", 0.0: "Real"}
 
-    def print_predicted(self,texts, predicted):
+    def print_predicted(self, texts, predicted):
         for i, p in enumerate(predicted):
             try:
                 t = texts[i][:30]
@@ -19,14 +20,13 @@ class detection:
                 t = texts[i]
 
             if p:
-                color = "\033[32;01m"
-            else:
                 color = "\033[31;01m"
+            else:
+                color = "\033[32;01m"
 
-            print(f'{i+1}-{t:^30}... : {color}{self.classes[p]}\033[00m')
+            print(f"{i+1}-{t:^30}... : {color}{self.classes[p]}\033[00m")
 
-        
-    def detect_one_text(self,text, with_title, verbose=True):
+    def detect_one_text(self, text, with_title, verbose=True):
         text = [text]
         if with_title:
             text = text_with_title(text)
@@ -35,7 +35,7 @@ class detection:
 
         predicted = self.model.predict(vectorized)
         if verbose:
-            print(self.classes[predicted[0]])
+            self.print_predicted(text, predicted)
         return predicted
 
     def detect(self, texts, with_title, verbose=False):
@@ -46,8 +46,10 @@ class detection:
 
         predicted = self.model.predict(vectorized)
         if verbose:
-            print(predicted)
+            self.print_predicted(texts, predicted)
+        return predicted
+
 
 # test
 det = detection()
-det.detect(["I write a test text","it is another text"], False)
+det.detect(["I write a test text", "it is another text"], False, True)
