ca291a6ebfb68d986098d5a3152872e8580efd69
meysam-kazemi <meisam.daysu@gmail.com>
2024-03-10 22:34:30 +0330

    class for detection

--- detection.py
+++ detection.py
@@ -8,18 +8,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score , confusion_matrix , ConfusionMatrixDisplay
 import pickle
 
-data_dir = "/run/media/meysam/PROGRAM/0.data/Fake News Detection Dataset/News _dataset"
-files = os.listdir(data_dir)
-files = list(map(lambda a:data_dir+"/"+a , files))
 
-fakes = pd.read_csv(files[0])
-trues = pd.read_csv(files[1])
-
-vectorizer = TfidfVectorizer()
-features = vectorizer.fit_transform(features)
-
-# Load the model and vectorizer
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-with open("Vectorizer.pkl","rb") as f:
-    vect = pickle.load(f)
+class detection:
+    def __init__(self):
+        with open('model.pkl', 'rb') as f:
+            self.model = pickle.load(f)
+        print('\033[33mModel Loaded!')
+        with open("Vectorizer.pkl","rb") as f:
+            self.vect = pickle.load(f)
+        print('\033[33mVectorizer Loaded!\033[00m')
+        
