b7534cb8fbfec0f4f598d73f667aa55a453e8549
meysam-kazemi <meisam.daysu@gmail.com>
2024-03-11 00:15:22 +0330

    delete unused packages from utils

--- utils.py
+++ utils.py
@@ -1,11 +1,3 @@
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score , confusion_matrix , ConfusionMatrixDisplay
 import pickle
 
 def load_vectorizer():
