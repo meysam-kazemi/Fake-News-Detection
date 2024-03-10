7dc6173f9ab7ba836846697366cde7113996b492
meysam-kazemi <meisam.daysu@gmail.com>
2024-03-10 23:04:12 +0330

    write predected

--- detection.py
+++ detection.py
@@ -1,18 +1,20 @@
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score , confusion_matrix , ConfusionMatrixDisplay
 import pickle
-
+from utils import text_with_title, load_vectorizer
 
 class detection:
     def __init__(self):
         with open('model.pkl', 'rb') as f:
             self.model = pickle.load(f)
         print('\033[33mModel Loaded!')
+
+        self.vect = load_vectorizer()
         
-    def 
+    def detect(self,text, with_title):
+        if with_title:
+            text = text_with_title(text)
+
+        vectorized = self.vect.transform(text)
+
+        predicted = self.model.predict(vectorized)
+        return predicted
+
