0acf8893862dbb83274ac1dfefe6c7ab6468e978
meysam-kazemi <meisam.daysu@gmail.com>
2026-06-24 10:00:00 +0330

    fix indentation and formatting in utils

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import pickle
 
+
 def load_vectorizer():
-    with open("Vectorizer.pkl","rb") as f:
-                vect = pickle.load(f)
+    with open("Vectorizer.pkl", "rb") as f:
+        vect = pickle.load(f)
     return vect
 
+
 def text_with_title(text):
-        res = text['title'] + " " + text["text"]
-        return res
+    res = text["title"] + " " + text["text"]
+    return res
